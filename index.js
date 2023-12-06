e1ddbc5d382d6a7c783af252bb1591a9d2110b0c
Artem Pankov <ae_apankov@idcontractors.net>
2023-12-07 00:11:39 +0100

    Added authentication flow via Google

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const PassportGoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+// URLS = {
+//     googleAuthURL: "/auth/google",
+// }
+
+
+const app = express();
+passport.use(new PassportGoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        // callbackURL: URLS.googleAuthURL + '/callback'
+        callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken);
+        console.log(refreshToken);
+        console.log(profile);
+        console.log(done);
+    }
+));
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        'scope': ['profile', 'email']
+    })
+)
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+)
 
 const PORT = process.env.PORT || 6464;
 app.listen(PORT);
